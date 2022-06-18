ba2f1b8ed10fbc3e771bcac88a63d206538418b7
fd54386 <69657670+fd54386@users.noreply.github.com>
2022-06-18 00:07:42 -0400

    Very clean solution, able to view default ticker chart reasonably well.
    Need to fix reactivity.

--- LeanUI/app.R
+++ LeanUI/app.R
@@ -1,48 +1,117 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+#Developed by Fred Davey
+#Custom Stock Data Browser -- Lean UI file
+#This project is intended to be used for custom indicator definitions and exploration of stocks utilizing statistical methods not available in common charting software.
+#Note-- there is no warranty provided with this software.  Any user who actively trades with this software is encouraged to do their own validations.
 
+#This file is for a lean UI to pair with a separate R instance that queries the Yahoo API, runs calculations, and populates a SQL Server Table
+
+
+#Imports
 library(shiny)
+library(plotly)
+library(tidyverse)
+library(tidyquant)
+library(quantreg)
+library(broom)
+library(RODBC)
 
-# Define UI for application that draws a histogram
+#####
+#Functions
+fnPullSQLStockData<- function(aConString, aSymbol){
+  fQuery = paste0("SELECT [Symbol]
+      ,[TradeTime]
+      ,[Last]
+      ,[Change]
+      ,[pctChange]
+      ,[Open]
+      ,[High]
+      ,[Low]
+      ,[Volume]
+      ,[appendTime]
+      ,[Slope]
+      ,case when [SloLoCI] < -50000 then NULL else SloLoCI end as SloLoCI
+	    ,case when SloUpCI > 50000 then NULL else SloUpCI end as SloUpCI
+	  ,Volume - lag(Volume,1) over(order by appendTime asc) as IntervalVolume
+  FROM [Stocks].[dbo].[YahooQuotesAndLADSlope]
+Where Symbol like '", 'SPY', "' and appendTime > cast(getdate() as Date)
+order by appendTime asc")
+  return(sqlQuery(connection, fQuery, 100))
+}
+
+#####
+#Global Variables
+connection = odbcConnect(dsn = 'Stock')
+
+#####
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  
+  # Application title
+  titlePanel(h1('Stock Scanner and Custom Chart Indicators'),
+      ),
+  
+  # Sidebar 
+  sidebarLayout(
+    sidebarPanel(
+      p('LastUpdated: ', textOutput('Title')),
+      #Text Input Box to Pull a Ticker
+      textInput('TickerSearch', label = 'Specify Ticker', value = "SPY")
+      
+      #List of everything pulled currently
+      #dataTableOutput("IndicatorList"),
+      
+      #Filters toggles for RS / RW / Volume
+      
+      #Toggle for auto refresh with timescale (seconds)
+      #Button for manual refresh
+      
+    ),
+    
+    # Plots
+    mainPanel(
+      
+      fluidRow( column(6, 
+      plotOutput("Chart")),
+      column(3, 
+      # Not available with historical setup at the moment (requires a lag reference)
+      plotOutput("VolumeProfile"))), 
+      fluidRow(column(6, 
+      plotOutput("IndicatorVsTime")))
+      
     )
+  )
 )
 
-# Define server logic required to draw a histogram
+#####
 server <- function(input, output) {
+  #Pull Day's data for our ticker
+  
+  output$Title<- renderText(as.character(Sys.time()))
+  
+  WorkingData <- fnPullSQLStockData(connection, input$TickerSearch)
+  
+  #Plot Ticker
+  output$Chart <- renderPlot({
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+        ggplot(data = WorkingData, aes(x = appendTime, y= Last)) +
+          geom_line() + geom_point() + labs(title = input$TickerSearch)
+      
+  })
+# 
+#   #Plot Volume Profile
+#   output$VolumeProfile <- renderPlot({
+#      ggplot(data = WorkingData[], aes(x = appendTime, y= Last))+
+#          geom_violin(aes(weight = IntervalVolume))
+#    })
+# 
+#   #Plot Indicators
+#   output$IndicatorVsTime <- renderPlot({
+#     #We get warnings for the 'null' data for the beginning points prior to indicator calculation
+# 
+#         ggplot(data = WorkingData[seq(1, nrow(WorkingData), 6), ], aes(x = appendTime, y= Slope)) +
+#           geom_point() + geom_ribbon(aes(ymin = SloLoCI, ymax = SloUpCI), alpha = .1) +
+#           labs(title = 'Robust Regression Slope vs Time') + geom_hline(aes(yintercept = 0))
+#   })
+      
 }
 
 # Run the application 
